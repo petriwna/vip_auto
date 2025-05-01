b440d62698cece1d764c242c5b62f4255a27ebcc
petriwna <petriwna43@gmail.com>
2025-05-01 14:36:40 +0300

    styling hero

--- src/js/Main.js
+++ src/js/Main.js
@@ -4,8 +4,6 @@ export class Main {
         this.menuBtnOpen = document.querySelector('.menu-btn-open');
         this.menuBtnClose = document.querySelector('.menu-btn-close');
         this.form = document.querySelector('#get-form');
-        this.banner = document.querySelector('#cookie-banner');
-        this.acceptBtn = document.querySelector('#accept-cookies');
 
         this.addEventListeners();
     }
@@ -14,15 +12,6 @@ export class Main {
         this.menuBtnOpen.addEventListener('click', this.handleMenuButton.bind(this));
         this.menuBtnClose.addEventListener('click', this.handleMenuButton.bind(this));
         this.form.addEventListener('submit', this.handleFormSubmit.bind(this));
-
-        if (!localStorage.getItem('cookiesAccepted')) {
-            this.banner.style.display = 'flex';
-        }
-
-        this.acceptBtn.addEventListener('click', () => {
-            localStorage.setItem('cookiesAccepted', 'true');
-            this.banner.style.display = 'none';
-        });
     }
 
 
